87d03944349c73c4cb6fdcbf66ad3ec083ac131f
ShahbazShaddy <shahbazghafil@gmail.com>
2025-04-06 14:18:18 +0500

    Add email configuration template and update .gitignore

---
+++ email_config_templete.py
@@ -0,0 +1,7 @@
+# Add your own Email Configuration and remain this file to email_config.py
+EMAIL_CONFIG = {
+    'HOST': 'smtp.gmail.com',
+    'PORT': 587,
+    'USER': 'your-gmail',  # Your Gmail address
+    'PASSWORD': 'your-app-password'   # App password without spaces
+}
